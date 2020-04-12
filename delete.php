fc8dee076a964f083e2e119118ce34e7e87546c7
andreiticu <63374530+andreiticu@users.noreply.github.com>
2020-04-12 11:41:44 +0300

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,6 +1,18 @@
 <?php
  //include connection file
  include "connection2.php";
+ $sqll1="DROP PROCEDURE deleteImage";
+$sqll2="CREATE PROCEDURE images.deleteimage
+(
+in sD int
+)
+begin 
+delete from images.images where id=sD;
+end;";
+$c1=$con->prepare($sqll1);
+$c2=$con->prepare($sqll2);
+$c1->execute();
+$c2->execute();
 $sql="CREATE TRIGGER after_delete AFTER DELETE ON images FOR EACH ROW
                 BEGIN
                 INSERT INTO images_updated(title,status,edtime)VALUES(OLD.title,'DELETED',NOW());
